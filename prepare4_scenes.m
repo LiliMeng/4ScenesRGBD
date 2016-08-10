08ddb6c1c6c36da3b814ed52989da37e3d6bede6
LiliMeng <lilimeng1103@gmail.com>
2016-08-09 17:20:53 -0700

    Update prepare4_scenes.m

--- prepare4_scenes.m
+++ prepare4_scenes.m
@@ -4,17 +4,17 @@ close all;
 
 N=1100
 
-posenet_format = '/Users/jimmy/Desktop/RGBD_4Scenes/kitchen/test.txt';
+posenet_format = '/Users/jimmy/Desktop/RGBD_4Scenes/apt1/living/data_test.txt';
 fileID = fopen(posenet_format,'w');
 
 
 
-for i = 1:357
-  poseRTFileName = sprintf('/Users/jimmy/Desktop/images/4_scenes/apt1/kitchen/data/frame-%06d.pose.txt', i-1);
+for i = 1:493
+  poseRTFileName = sprintf('/Users/jimmy/Desktop/images/4_scenes/apt1/living/data/frame-%06d.pose.txt', i-1);
   PoseRT=load(poseRTFileName);
   PoseTQ=PoseRT2PoseTQ(PoseRT);  
   rgb_filename=sprintf('image/frame-%06d.color.png', i-1);
-  fprintf(fileID, '%s %.10f %.10f %.10f %10f %.10f %.10f %10f\n', rgb_filename, PoseQT(1),PoseQT(2),PoseQT(3),PoseQT(4),PoseQT(5),PoseQT(6),PoseQT(7));
+  fprintf(fileID, '%s %.10f %.10f %.10f %10f %.10f %.10f %10f\n', rgb_filename, PoseTQ(1),PoseTQ(2),PoseTQ(3),PoseTQ(4),PoseTQ(5),PoseTQ(6),PoseTQ(7));
 end
 fclose(fileID);
      
