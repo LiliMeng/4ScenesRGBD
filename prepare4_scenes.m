1d5dee3c8d9e37d00a9ffdb167c7b5a539a2ff6b
LiliMeng <lilimeng1103@gmail.com>
2016-08-16 20:28:55 -0700

    Update prepare4_scenes.m

--- prepare4_scenes.m
+++ prepare4_scenes.m
@@ -2,20 +2,24 @@ clear all;
 close all;
 
 
-N=1100
 
-posenet_format = '/Users/jimmy/Desktop/RGBD_4Scenes/apt1/living/data_test.txt';
+posenet_format = '/home/lci/workspace/4scenes/apt2/bed/data_test.txt';
 fileID = fopen(posenet_format,'w');
 
 
+invalid_matrix=-ones(4);
 
-for i = 1:493
-  poseRTFileName = sprintf('/Users/jimmy/Desktop/images/4_scenes/apt1/living/data/frame-%06d.pose.txt', i-1);
+for i = 1:244
+  poseRTFileName = sprintf('/home/lci/workspace/4scenes/apt2/bed/data/frame-%06d.pose.txt', i-1)
   PoseRT=load(poseRTFileName);
-  PoseTQ=PoseRT2PoseTQ(PoseRT);  
+  tf=isequal(PoseRT, invalid_matrix);
+  if tf==1
+        continue;
+  end
+  PoseTQ=PoseRT2PoseTQ(PoseRT);
   rgb_filename=sprintf('image/frame-%06d.color.png', i-1);
   fprintf(fileID, '%s %.10f %.10f %.10f %10f %.10f %.10f %10f\n', rgb_filename, PoseTQ(1),PoseTQ(2),PoseTQ(3),PoseTQ(4),PoseTQ(5),PoseTQ(6),PoseTQ(7));
 end
+
 fclose(fileID);
      
-
